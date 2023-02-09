373ce48379780750a503608cc518d5f48bbe42a2
kimwoojin <waterather21@gaeasoft.co.kr>
2023-02-09 17:14:26 +0900

    Initial Commit

---
+++ FireMap/FireMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireMapApp.swift
+//  FireMap
+//
+//  Created by KimWooJin on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct FireMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
